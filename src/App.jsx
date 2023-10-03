e33a6f512f64eddf254b442ce0ae3530a65ea380
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-10-03 19:06:25 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from './Components/Button.style';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click this button</Button>
+        <Button>Click this button</Button>
+        <Button>Click this button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
